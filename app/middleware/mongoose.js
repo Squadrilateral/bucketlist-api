eb51b75457bc26b77f0ac5c33e1d85e3fa62abb0
dannyclark77 <dannyclark77@gmail.com>
2017-10-11 10:04:06 -0400

    Initial commit

---
+++ app/middleware/mongoose.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const database = require('config/database')
+const mongoose = require('mongoose')
+const uri = process.env.MONGODB_URI || database.mongoose.uri
+mongoose.Promise = global.Promise
+mongoose.connect(uri, {
+  useMongoClient: true
+})
+
+module.exports = mongoose
