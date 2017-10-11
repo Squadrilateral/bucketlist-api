fff06880c5d244fc2cd0cee1130efea725e84fe8
ccasner <charlottecasner@gmail.com>
2017-10-11 13:01:48 -0400

    Edits curl scripts for listitem CRUD
    Adds successful curl scripts
    Added an .env file (not committed) to store keys
    
    Contributors:
    Danny C, Charlotte C

--- scripts/listitems/update.sh
+++ scripts/listitems/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/listitems"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -9,8 +9,11 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
-    "example": {
-      "text": "'"${TEXT}"'"
+    "listitem": {
+      "name": "'"${NAME}"'",
+      "location": "'"${LOCATION}"'",
+      "category": "'"${CATEGORY}"'",
+      "rating": "'"${RATING}"'"
     }
   }'
 
